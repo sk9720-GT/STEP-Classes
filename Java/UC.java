fff05a889afce92775622aa5143e84330c86ee2d
SK9720-GT <sk9720@srmist.edu.in>
2026-02-17 13:43:27 +0530

    UC3 submission initial

--- Java/UC.java
+++ Java/UC.java
@@ -1,14 +1,14 @@
 public class UC {
     public static void main(String[] args){
         System.out.println("OOPS");
-        
-        System.out.println("   ***   " + " " + "   ***   " + " " + "*********" + " " + "*********");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + "***    **" + " " + "*********");
-        System.out.println("**     **" + " " + "**     **" + " " + "***    **" + " " + "**       ");
-        System.out.println("*       *" + " " + "*       *" + " " + "*********" + " " + "*********");
-        System.out.println("**     **" + " " + "**     **" + " " + "***      " + " " + "       **");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + "***      " + " " + "*********");
-        System.out.println("   ***   " + " " + "   ***   " + " " + "***      " + " " + "*********");
+
+        System.out.println(String.join(" ","   ***   ", "   ***   ", "*********", "*********"));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "***    **", "*********"));
+        System.out.println(String.join(" ", "**     **", "**     **", "***    **", "**       "));
+        System.out.println(String.join(" ", "*       *", "*       *", "*********", "*********"));
+        System.out.println(String.join(" ", "**     **", "**     **", "***      ", "       **"));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "***      ", "*********"));
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", "***      ", "*********"));
         
     }
 }
